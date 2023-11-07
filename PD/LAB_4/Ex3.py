8609c6e1e413ade058d948346e18a675c167006b
Adrian Chihai <cadrian1602@yahoo.com>
2023-11-07 12:45:30 +0200

    t

--- PD/LAB_4/Ex3.py
+++ PD/LAB_4/Ex3.py
@@ -11,5 +11,5 @@ print("Dif -> ", diferenta(nr1, nr2))
 resSuma = list(map(suma, [2, 2], [nr1, nr2]))
 resDiferenta = list(map(diferenta, [0, 0], [nr1, nr2]))
 
-print(f"Suma ->  {resSuma}")
+print(f"Suma -> {resSuma}")
 print(f"Diferenta -> {resDiferenta}")
