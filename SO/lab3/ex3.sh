4954f2fceccf26d934404dd56bf00dbe131ef17a
ChihaiAdrian <cadrian@yahoo.com>
2023-11-17 11:01:10 +0200

    SO

--- SO/lab3/ex3.sh
+++ SO/lab3/ex3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Acest program va rula pana vei apasa pe 'q'"
 my_string=""
-
+nr=0
 while true; do
 	read -n 1 char
 	if [ "$char" == "q" ]
@@ -9,7 +9,8 @@ while true; do
 		break
 	fi
 	my_string="${my_string}${char}"
+	((nr++))
 done
 
 echo -e "\nAi introdus acest sir: $my_string"
-
+echo -e "\nAre $nr caractere"
