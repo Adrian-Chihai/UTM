1a677f9707f6357bc674750a1f03566483f6b4c3
ChihaiAdrian <cadrian1602@yahoo.com>
2024-02-18 13:40:26 +0200

    lab 1 inginerie inversa

---
+++ anul_3/s_2/TII/lab1/password/password/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hash = "9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08";
+            
+            Console.WriteLine("Enter your password");
+
+            string password = HideUserInput();
+
+            if (HashPassword(password).ToUpperInvariant().Equals(hash))
+            {
+                Console.WriteLine("Access Granted!");
+            }
+            else
+            {
+                Console.WriteLine("Access Denied!");
+            }
+
+        }
+
+        static string HashPassword(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    builder.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        static string HideUserInput()
+        {
+            string input = "";
+
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(intercept: true);
+
+                if (key.Key != ConsoleKey.Enter)
+                {
+                    input += key.KeyChar;
+                    Console.Write("*");
+                }
+                
+            } while (key.Key != ConsoleKey.Enter);
+            
+            Console.WriteLine();
+
+            return input;
+        }
+    }
+}
