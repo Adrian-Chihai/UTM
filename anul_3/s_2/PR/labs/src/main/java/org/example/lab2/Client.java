284a59684066626dbbfd92c93d4329a08a9e70db
Adrian Chihai <cadrian1602@yahoo.com>
2024-02-28 10:24:07 +0200

    labs

--- anul_3/s_2/PR/labs/src/main/java/org/example/lab2/Client.java
+++ anul_3/s_2/PR/labs/src/main/java/org/example/lab2/Client.java
@@ -1,2 +1,35 @@
-package org.example.lab2;public class Client {
+package org.example.lab2;
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client implements Runnable {
+    private Scanner input;
+    private String nickname;
+    private DatagramSocket clientSocket;
+    private InetAddress inetAddress;
+    private int port;
+
+    public Client(int port) throws SocketException, UnknownHostException {
+        input = new Scanner(System.in);
+        clientSocket = new DatagramSocket();
+        inetAddress = InetAddress.getByName("localhost");
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    private void enterNickname() {
+        System.out.println("Enter your nickname: ");
+        nickname = input.nextLine();
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- anul_3/s_2/PR/labs/src/main/java/org/example/lab2/Server.java
+++ anul_3/s_2/PR/labs/src/main/java/org/example/lab2/Server.java
@@ -1,2 +1,49 @@
-package org.example.lab2;public class Server {
+package org.example.lab2;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Server implements Runnable {
+    private int serverPort;
+    private DatagramSocket serverSocket;
+    private byte[] receivedData;
+    private DatagramPacket receivedPacket;
+    private boolean alive;
+
+    public Server(int serverPort) throws SocketException {
+        this.serverPort = serverPort;
+        serverSocket = new DatagramSocket(serverPort);
+        receivedData = new byte[1024];
+    }
+
+    @Override
+    public void run() {
+        while (!alive) {
+            receivedPacket = new DatagramPacket(receivedData, receivedData.length);
+            try {
+                serverSocket.receive(receivedPacket);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            InetAddress inetAddress = receivedPacket.getAddress();
+            int port = receivedPacket.getPort();
+            receivedPacket = new DatagramPacket(receivedData, receivedData.length, port);
+            String clientMessage = new String(receivedPacket.getData(), 0, receivedPacket.getLength());
+            System.out.println(inetAddress + ": " + clientMessage);
+
+            try {
+                serverSocket.send(receivedPacket);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws SocketException {
+        Server server = new Server(8080);
+        server.run();
+    }
 }
